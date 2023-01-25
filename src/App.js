abef0fc3f9de5d134002ac6b9642a0b57c3f677e
Gyanendra Knojiya <gyanendrak064@gmail.com>
2023-01-25 18:58:48 +0530

    Local storage store added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
-import "./App.css";
-import Main from "./components/Main";
-import TodoContext from "./ToDoContext";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Main from './components/Main';
+import TodoContext from './ToDoContext';
 
 function App() {
-  const [list, setList] = useState(["Start adding your todo"]);
+  const [list, setList] = useState(['Start adding your todo']);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const storedTodoList = localStorage.getItem('todo_list');
+    if (storedTodoList) {
+      setList(JSON.parse(storedTodoList));
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem('todo_list', JSON.stringify(list));
+    }
+  }, [list, isLoading]);
+
   return (
     <div>
       <TodoContext.Provider value={[list, setList]}>
-        <Main />
+        {isLoading ? <p>Loading...</p> : <Main />}
       </TodoContext.Provider>
     </div>
   );
